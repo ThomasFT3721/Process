44f5b574ca59b86ba6e8f7286e05a1b12e78f945
Thomas FONTAINE--TUFFERY <78264430+ThomasFT3721@users.noreply.github.com>
2024-01-04 13:06:58 +0000

    Test github page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Process
 
 - DevOps
-    - [Serveur Apache avec redirection vers des conteneurs Docker](./DevOps/serveur_apache_avec_redirection_vers_des_conteneurs_docker.md)
+    - [Serveur Apache avec redirection vers des conteneurs Docker](./DevOps/serveur_apache_avec_redirection_vers_des_conteneurs_docker_sur_aws_lightsail.md)
---
+++ scripts.js
@@ -0,0 +1,323 @@
+class HttpRequest {
+    static #baseURL = './';
+    static #headers = {};
+
+    static async get(url) {
+        const response = await fetch(`${this.#baseURL}${url}`, {
+            method: 'GET',
+            headers: this.#headers
+        });
+        return await response.text();
+    }
+}
+
+class Data {
+    static tags = [];
+    static process = {};
+    static menu = [];
+
+    static async load() {
+        const data = await HttpRequest.get('data.json');
+        const json = JSON.parse(data);
+        this.tags = json.tags;
+        this.process = {};
+        this.menu = [];
+
+        Object.keys(json.process).forEach(key => {
+            const process = json.process[key];
+            this.process[key] = new Process(process.url, process.title, process.tags);
+        });
+
+        for (const item of json.menu) {
+            this.menu.push(new MenuItem(item.type, item.name, item.url, item.items));
+        }
+    }
+}
+
+class Process {
+    url;
+    title;
+    tags;
+
+    constructor(url, title, tags) {
+        this.url = url;
+        this.title = title;
+        this.tags = tags;
+    }
+}
+
+class MenuItem {
+    type;
+    name;
+    url;
+    items;
+
+    constructor(type, name, url, items) {
+        this.type = type;
+        this.name = name;
+        this.url = url;
+        if (items) {
+            this.items = [];
+            for (const item of items) {
+                this.items.push(new MenuItem(item.type, item.name, item.url, item.items));
+            }
+        }
+    }
+
+    isItem() {
+        return this.type === 'item';
+    }
+}
+
+window.currentItemActive = null;
+
+window.buildItemOfMenu = function (item, withProcess = false) {
+    if (item === null) {
+        let items = '';
+        for (const subItem of Data.menu) {
+            items += buildItemOfMenu(subItem, withProcess);
+        }
+        return items;
+    }
+    if (!item.isItem()) {
+        let items = '';
+        for (const subItem of item.items) {
+            items += buildItemOfMenu(subItem, withProcess);
+        }
+        return `<div class="item item-group">
+                    <a href="#${item.url}" class="name ${window.currentItemActive === item.url ? "active" : ""}" link="close-menu">${item.name}</a>
+                    ${items !== "" ? `<div class="items">` + items + "</div>" : ""}
+               </div>`;
+    } else {
+        if (withProcess) {
+            return `<div class="item item-process">
+                        <a href="#${item.url}" class="name ${window.currentItemActive === item.url ? "active" : ""}" link="close-menu">${item.name}</a>
+                    </div>`;
+        }
+    }
+    return '';
+}
+window.buildMenu = function () {
+    const menu = document.querySelector('#menu');
+    let items = `
+                        <div id="menu-handler">
+                            <span id="line1"></span>
+                            <span id="line2"></span>
+                            <span id="line3"></span>
+                        </div>
+                        <div class="title">Menu</div>
+                        <div class="elements">
+                        <div class="item">
+                            <a href="#" class="name ${window.currentItemActive === "" ? "active" : ""}" link="close-menu">Accueil</a>
+                        </div>
+                       `;
+    for (const item of Data.menu) {
+        items += buildItemOfMenu(item, false);
+    }
+    menu.innerHTML = items + "</div>";
+}
+window.getItemAndParent = function (url) {
+    const getItemAndParentOfMenu = function (item, url) {
+        if (item.url === url) {
+            return item;
+        }
+        if (item.items) {
+            for (const subItem of item.items) {
+                const result = getItemAndParentOfMenu(subItem, url);
+                if (result) {
+                    if (result.parent) {
+                        return result;
+                    } else {
+                        return {
+                            item: subItem,
+                            parent: item
+                        };
+                    }
+                }
+            }
+        }
+        return null;
+
+    }
+
+    for (const item of Data.menu) {
+        const result = getItemAndParentOfMenu(item, url);
+        if (result) {
+            if (result.parent) {
+                return result;
+            } else {
+                return {
+                    item: result,
+                    parent: null
+                };
+            }
+        }
+    }
+    return null;
+}
+
+window.getBreadcrumb = function (itemAndParent, breadcrumb = []) {
+    breadcrumb.push(itemAndParent.item);
+    if (itemAndParent.parent) {
+        return getBreadcrumb(getItemAndParent(itemAndParent.parent.url), breadcrumb);
+    }
+    breadcrumb.shift();
+    breadcrumb.push({
+        name: "Accueil",
+        url: ""
+    });
+    breadcrumb = breadcrumb.reverse();
+    return breadcrumb;
+}
+
+window.buildContent = async function (withHeader = true) {
+    const app = document.querySelector('#app');
+    const header = document.querySelector('#header');
+    const content = document.querySelector('#content');
+    let htmlHeader = '';
+    let htmlContent = '';
+    if (window.currentItemActive) {
+        const converter = new showdown.Converter();
+        const itemAndParent = getItemAndParent(window.currentItemActive);
+        if (itemAndParent.item.isItem()) {
+            app.setAttribute('type', 'process');
+            const process = Data.process[itemAndParent.item.url];
+            if (process) {
+                htmlHeader = `
+                            <div class="title">${process.title}</div>
+                            <div class="line">
+                                <div class="breadcrumb">${getBreadcrumb(itemAndParent).map(item => `<a class="item" href="#${item.url}" link>${item.name}</a>`).join(`<span>/</span>`)}</div>
+                                <div class="tags">${process.tags.map(tag => `<div class="tag">${tag}</div>`).join('')}</div>
+                            </div>
+                           `;
+                htmlContent = `<div class="process">${converter.makeHtml(await HttpRequest.get(process.url))}</div>`;
+            } else {
+                htmlHeader = `
+                        <div class="title">Process not found</div>
+                        <div class="breadcrumb">${getBreadcrumb(itemAndParent).map(item => `<a class="item" href="#${item.url}" link>${item.name}</a>`).join(`<span>/</span>`)}</div>
+                       `;
+            }
+        } else {
+            app.setAttribute('type', 'group');
+            htmlHeader = `
+                    <div class="line" id="search-container">
+                        <div class="title">${itemAndParent.item.name}</div>
+                    </div>
+                    <div class="breadcrumb">${getBreadcrumb(itemAndParent).map(item => `<a class="item" href="#${item.url}" link>${item.name}</a>`).join(`<span>/</span>`)}</div>
+                   `;
+            htmlContent = `<div class="menu delete-first">${buildItemOfMenu(itemAndParent.item, true)}</div>`;
+        }
+    } else {
+        app.setAttribute('type', 'group');
+        htmlHeader = `
+                <div class="line" id="search-container">
+                    <div class="title">Accueil</div>
+                </div>
+               `;
+        htmlContent = `<div class="menu main">${buildItemOfMenu(null, true)}</div>`;
+    }
+    content.innerHTML = htmlContent;
+    if (withHeader) {
+        header.innerHTML = htmlHeader;
+        buildSearch();
+    }
+}
+
+window.buildSearch = function () {
+    const search = document.querySelector('#search');
+    if (search !== null) {
+        search.remove();
+    }
+    if (document.querySelector('#app').getAttribute('type') === 'group') {
+        let searchValue = '';
+        const cookies = document.cookie.split(';');
+        for (const cookie of cookies) {
+            const index = cookie.indexOf('=');
+            if (index !== -1) {
+                const key = cookie.substring(0, index).trim();
+                const value = cookie.substring(index + 1).trim();
+                if (key === 'search') {
+                    searchValue = value;
+                }
+            }
+        }
+
+        document.querySelector("#search-container").insertAdjacentHTML('beforeend', `<input id="search" type="text" placeholder="Rechercher" value="${searchValue}"/>`);
+
+        const search = document.querySelector('#search');
+        search.addEventListener('keyup', () => {
+            const value = search.value;
+            document.cookie = `search=${value}`;
+            applySearch();
+        });
+    }
+}
+
+window.applySearch = function () {
+    buildContent(false);
+    if (document.querySelector('#app').getAttribute('type') === 'group') {
+        let searchValue = '';
+        const cookies = document.cookie.split(';');
+        for (const cookie of cookies) {
+            const index = cookie.indexOf('=');
+            if (index !== -1) {
+                const key = cookie.substring(0, index).trim();
+                const value = cookie.substring(index + 1).trim();
+                if (key === 'search') {
+                    searchValue = value;
+                }
+            }
+        }
+        searchValue = searchValue.toLowerCase();
+
+        if (searchValue !== '') {
+            document.querySelectorAll('.item-process').forEach(item => {
+                const name = item.querySelector('a').innerText.toLowerCase();
+                if (name) {
+                    if (name.indexOf(searchValue) === -1) {
+                        item.remove();
+                    }
+                }
+            });
+
+            document.querySelectorAll('.item-group').forEach(item => {
+                const notHide = item.querySelector('.item-process:not(.hide)');
+                if (notHide === null) {
+                    item.remove();
+                }
+            });
+        }
+    }
+}
+
+window.init = function () {
+    buildMenu();
+    buildContent();
+    applySearch();
+    setTimeout(() => {
+        document.querySelectorAll('*[link]').forEach(item => {
+            item.addEventListener('click', () => {
+                window.currentItemActive = item.getAttribute('href').substring(1);
+                document.querySelector('#menu').classList.remove('open');
+                init();
+            });
+        });
+        document.querySelector('#menu-handler').addEventListener('click', () => {
+            document.querySelector('#menu').classList.toggle('open');
+        });
+    }, 100);
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    //Get current item active
+    const url = window.location.href;
+    const index = url.indexOf('#');
+    if (index !== -1) {
+        window.currentItemActive = url.substring(index + 1);
+    } else {
+        window.currentItemActive = '';
+    }
+    //Load data
+    await Data.load();
+    init();
+});
